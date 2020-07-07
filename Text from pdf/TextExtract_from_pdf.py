8570c25e57596ea35a42e2c11e76dd7da6a94566
Gokul Ballava Mishra <67966130+Gokulmishra@users.noreply.github.com>
2020-07-07 23:49:16 +0530

    Update TextExtract_from_pdf.py

--- Text from pdf/TextExtract_from_pdf.py
+++ Text from pdf/TextExtract_from_pdf.py
@@ -1,8 +1,18 @@
 # extract text from pdffile
+# by @Gokulmishra
+
 import PyPDF2
-pdfFile=open('fourier.pdf','rb')
+
+pdfFile=open('SturmLivouliee.pdf','rb')
+
+#input your pdf file name instead of SturmLivouliee like 'name.pdf' format ...
+# ... in thded same folder where the code is saved 
+
 pdfReader= PyPDF2.PdfFileReader(pdfFile)
 print(pdfReader.numPages)
 pgObj=pdfReader.getPage(0)
+
+#to get different page change index number of getPage
+
 print(pgObj.extractText())
 pdfFile.close()
